3fc2e1cd2a62a0414022b3a11fb0354d4cbf9124
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-22 21:16:18 +0600

    Explored divisors

--- divisors.cpp
+++ divisors.cpp
@@ -17,12 +17,30 @@ int main()
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << i << '\n';
+    //     }
+    // }
+
+    vector<int> divisors;
+
+    for (int i = 1; i <= sqrt(n); i++)
     {
-        if (n % i == 0)
+        divisors.push_back(i);
+
+        if (n / i != i)
         {
-            cout << i << '\n';
+            divisors.push_back(n / i);
         }
+    }
+
+    sort(divisors.begin(), divisors.end());
+    for (auto integerValue : divisors)
+    {
+        cout << integerValue << " ";
     }
 
     return 0;
