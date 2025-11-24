9caaed1a4b0bcdfd5249eac05072d4879932962f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-24 19:14:32 +0600

    Solving Joyty and Chocolate problem

---
+++ C_Joty_and_Chocolate.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <class T> using pbds = tree<T, null_type,
+less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, a, b, p, q;
+  cin >> n >> a >> b >> p >> q;
+
+  
+
+  return 0;
+}
