7e4c4213a0bac25a4c69d95364613d47205d7226
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-24 21:02:03 +0600

    Solved first problem

--- C_Joty_and_Chocolate.cpp
+++ C_Joty_and_Chocolate.cpp
@@ -5,17 +5,34 @@
 using namespace __gnu_pbds;
 using namespace std;
 
-template <class T> using pbds = tree<T, null_type,
-less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template <class T>
+using pbds = tree<T, null_type,
+                  less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-int main(){
+int main()
+{
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int n, a, b, p, q;
+  long long n, a, b, p, q;
   cin >> n >> a >> b >> p >> q;
 
-  
+  long long countRed = 0;
+  long long countBlue = 0;
+
+  for (int i = 1; i <= n; i++)
+  {
+    if (i % a == 0)
+    {
+      countRed++;
+    }
+    else if (i % b == 0)
+    {
+      countBlue++;
+    }
+  }
+
+  cout << (countRed * p) + (countBlue * q);
 
   return 0;
 }
