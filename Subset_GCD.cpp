ab75cab79afbbb8ef3499960cedb7d1a7aa93901
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-12-06 16:50:36 +0600

    Solved Subset GCD problem - M

---
+++ Subset_GCD.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <class T>
+using pbds = tree<T, null_type,
+                  less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        int goal = n / k;
+        int stop = k;
+        for (int i = goal; stop > 0; i = i + goal)
+        {
+            cout << i << " ";
+            stop--;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
