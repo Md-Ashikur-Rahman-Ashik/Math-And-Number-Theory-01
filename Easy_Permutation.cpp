7db0bf3ab8c625222f8106913334b7159b7fb57b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-12-24 20:06:46 +0600

    Solved Easy Permutation problem - Q

---
+++ Easy_Permutation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+
+        for (int i = N; i >= 1; i--)
+        {
+            cout << i << " ";
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
